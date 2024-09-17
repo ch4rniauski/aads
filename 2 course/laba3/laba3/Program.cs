4951eeebc2cecef64334352496bbe4cc56dc3dec
Yauheni Charniauski <chernyavskij.z@mail.ru>
2024-09-17 20:56:17 +0300

    Add IncreaseHashtable function and edit some functions

--- 2 course/laba3/laba3/Program.cs
+++ 2 course/laba3/laba3/Program.cs
@@ -11,7 +11,24 @@ namespace laba3
 
             string str = Console.ReadLine();
 
-            Dictionary<char, int>[] hashTable = new Dictionary<char, int>[1];
+            HashElement[] hashTable = new HashElement[1];
+
+            foreach (char ch in str)
+                hashTable = AddToHashTable(hashTable, ch);
+
+            Console.WriteLine("1");
+        }
+
+        class HashElement
+        {
+            public char Symbol { get; set; }
+            public int Amount { get; set; }
+
+            public HashElement(char symbol, int amount = 1)
+            {
+                Symbol = symbol;
+                Amount = amount;
+            }
         }
 
         static int HashCode(char ch)
@@ -19,47 +36,49 @@ namespace laba3
             return (int)ch * 49 / 100;
         }
 
-        static bool HashTableContains(char ch, Dictionary<char, int>[] hashTable)
+        static bool HashTableContains(char ch, HashElement[] hashTable)
         {
             int bucket = HashCode(ch) % hashTable.Length;
 
-            if (hashTable[bucket][ch] == ch)
+            if (hashTable[bucket] != null && hashTable[bucket].Symbol == ch)
                 return true;
 
             return false;
         }
 
-        static Dictionary<char, int>[] AddToHashTable(Dictionary<char, int>[] hashTable, char ch)
+        static HashElement[] AddToHashTable(HashElement[] hashTable, char ch)
         {
             if (HashTableContains(ch, hashTable))
             {
                 int bucket = HashCode(ch) % hashTable.Length;
 
-                hashTable[bucket][ch]++;
+                hashTable[bucket].Amount++;
             }
 
             else
             {
                 if (hashTable[HashCode(ch) % hashTable.Length] != null)
-                {
-                    Dictionary<char, int>[] tempHashTable = new Dictionary<char, int>[hashTable.Length * 2];
-
-                    for (int i = 0; i < hashTable.Length; i++)
-                    {
-                        int bucket = HashCode(ch) % tempHashTable.Length;
-
-                        tempHashTable[bucket] = hashTable[i];
-
-                        hashTable = tempHashTable;
-                    }
-                }
+                    hashTable = IncreaseHashtable(hashTable);
 
                 int bucketNumber = HashCode(ch) % hashTable.Length;
 
-                hashTable[bucketNumber] = new Dictionary<char, int>();
-                hashTable[bucketNumber][ch] = 1;
+                hashTable[bucketNumber] = new HashElement(ch);
             }
             return hashTable;
+        }
+
+        static HashElement[] IncreaseHashtable(HashElement[] hashTable)
+        {
+            HashElement[] newHashTable = new HashElement[hashTable.Length * 2];
+
+            for (int i = 0; i < hashTable.Length; i++)
+            {
+                int bucket = HashCode(hashTable[i].Symbol) % newHashTable.Length;
+
+                newHashTable[bucket] = hashTable[i];
+            }
+
+            return newHashTable;
         }
     }
 }
